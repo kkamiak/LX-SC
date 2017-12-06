6641c52a51272ef2f411273ccf209db87d600cf2
Belko <siarhei.belko@instinctools.ru>
2017-12-06 14:39:59 +0300

    remove warnings

--- contracts/helpers/UserProxyTester.sol
+++ contracts/helpers/UserProxyTester.sol
@@ -5,11 +5,11 @@ contract UserProxyTester {
         return _someInputValue;
     }
 
-    function unsuccessfullFunction(bytes32 _someInputValue) returns(bytes32) {
+    function unsuccessfullFunction(bytes32) returns(bytes32) {
         revert();
     }
 
-    function forward(address _destination, bytes _data, uint _value, bool _throwOnFailedCall) returns(bytes32) {
+    function forward(address, bytes, uint, bool) returns(bytes32) {
         return 0x3432000000000000000000000000000000000000000000000000000000000000;
     }
 }
